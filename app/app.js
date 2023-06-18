aad754749f75769828597c2f846eeba2cc92da7f
KSA9110 <104611263+KSA9110@users.noreply.github.com>
2023-06-18 22:56:55 +0900

    프론트단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static( __dirname + '/src/public/')); //js파일에 접근 가능하도록 정적경로를 추가
+//__dirname은 app.js 파일이 있는 경로를 반환
+//static : 정적인 파일이 위치할 디렉토리를 지정하는 기능?정도로 이해하기.
+//백틱(``)은 ${}가 정상인식될 때 필요하다.
 
 //.use() : 미들웨어 등록하는 메서드
 app.use("/", home);
