4a6fbf85d2e72be50de5fb3bf3ff16671f69efb0
KSA9110 <104611263+KSA9110@users.noreply.github.com>
2023-06-19 00:46:27 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -24,7 +24,16 @@ fetch("/login", {
   },
   body: JSON.stringify(req)
 }).then((res) => res.json())
-  .then(console.log();
+  .then((res) => {
+    if(res.success) {
+      location.href = "/"; //res값이 성공이면 루트로 보내버린다
+    } else { //res값이 실패이면 res 안의 msg 값을 전달하여 알람을 보냄
+      alert(res.msg);
+    }
+  })
+  .catch((err) => {
+    console.error(new Error("로그인 중 에러 발생"));
+  });
 }
 
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,7 +7,8 @@ const ctrl = require("./home.ctrl")
 
 router.get("/", ctrl.output.hello);
  router.get("/login", ctrl.output.login);
- router.post("/login", ctrl.process.login); //프론트가 전달한 로그인데이터를 받아서 로그인기능을 처리하는 API임.
+ router.post("/login", ctrl.process.login); 
+ //프론트가 전달한 로그인데이터를 받아서 로그인기능을 처리하는 API임.
 
 
 module.exports = router;
