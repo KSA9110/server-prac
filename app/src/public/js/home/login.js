b35a353d9c3b2bccb979d7d75a8010bbf5780336
KSA9110 <104611263+KSA9110@users.noreply.github.com>
2023-06-18 23:46:13 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,15 +1,30 @@
 "use strict";
 
 const id = document.querySelector("#id"), // #을 붙이면 id값에 담긴 선택자를 가져온다는 뜻.
-  psword = document.querySelector("#pwword"),
+  psword = document.querySelector("#psword"),
 loginBtn = document.querySelector("button"); //button 태그를 통째로 불러오는거임.
   
 
 loginBtn.addEventListener("click", login);
 
-function login(){
-console.log("bye");
+function login() {
+const req = {
+  id: id.value,
+  psword: psword.value,
 };
+
+console.log(req);
+console.log(JSON.stringify(req));
+
+
+fetch("/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(req)
+});
+}
 
 
 
