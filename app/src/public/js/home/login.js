49cb692570a925539cd1fc7f857b0c5ab8305ef4
KSA9110 <104611263+KSA9110@users.noreply.github.com>
2023-06-18 23:23:24 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("하이");
-console.log("바이");
+const id = document.querySelector("#id"), // #을 붙이면 id값에 담긴 선택자를 가져온다는 뜻.
+  psword = document.querySelector("#pwword"),
+loginBtn = document.querySelector("button"); //button 태그를 통째로 불러오는거임.
+  
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+console.log("bye");
+};
+
+
+
